65b582fc4fc7254dc010602d39d91ffefe750119
aman godara <amangodara5686@gmail.com>
2024-10-19 11:03:22 +0530

    testing

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -1,34 +1,33 @@
-// middleware/multerConfig.js
-const multer = require('multer');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid'); // Import the UUID function
+const multer = require("multer");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './uploads'); // Define where to store the images
-    },
-    filename: (req, file, cb) => {
-        // const uniqueFilename = Date.now() + path.extname(file.originalname); 
-        const uniqueFilename = `${Date.now()}-${uuidv4()}${path.extname(file.originalname)}`; // Generate the unique filename with Date.now() and UUID
-        req.savedFilename = uniqueFilename; 
-        cb(null, uniqueFilename); 
-        // cb(null, Date.now() + path.extname(file.originalname)); // File naming convention
-    },
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    const uniqueFilename = `${Date.now()}-${uuidv4()}${path.extname(
+      file.originalname
+    )}`;
+    req.savedFilename = uniqueFilename;
+    cb(null, uniqueFilename);
+  },
 });
 
 const fileFilter = (req, file, cb) => {
-    const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
-    if (allowedTypes.includes(file.mimetype)) {
-        cb(null, true);
-    } else {
-        cb(new Error('Only .jpeg, .jpg, and .png formats allowed!'), false);
-    }
+  const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only .jpeg, .jpg, and .png formats allowed!"), false);
+  }
 };
 
 const upload = multer({
-    storage,
-    limits: { fileSize: 1024 * 1024 * 10 }, // Limit file size to 5MB
-    fileFilter,
+  storage,
+  limits: { fileSize: 1024 * 1024 * 10 },
+  fileFilter,
 });
 
 module.exports = upload;
---
+++ prisma/migrations/20241019052425_added_role_and_department_relationship_with_staff/migration.sql
@@ -0,0 +1,5 @@
+-- AddForeignKey
+ALTER TABLE "Staff" ADD CONSTRAINT "Staff_departmentId_fkey" FOREIGN KEY ("departmentId") REFERENCES "Department"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Staff" ADD CONSTRAINT "Staff_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE SET NULL ON UPDATE CASCADE;
