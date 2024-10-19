64bca5d23f24364f345234c8f8641d1109b82f87
Shukal-01 <131428954+Shukal-01@users.noreply.github.com>
2024-10-19 14:39:10 +0530

    changes in index file

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use("/", rootRouter);
+app.use("/api/", rootRouter);
 
 
 app.listen(PORT, () => {
