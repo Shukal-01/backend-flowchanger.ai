f89e4c18386f1250590c57c487d31ff2731900f6
aman godara <amangodara5686@gmail.com>
2024-10-22 10:01:42 +0530

    stashed changes

--- router/admin/customDetails.router.js
+++ router/admin/customDetails.router.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD:router/admin/customDetails.router.js
-const express = require('express');
-const customDetailsRouter = express.Router();
-const customDetailsController = require("../../controller/admin/staff/customDetails.controller");
-
-customDetailsRouter.post('/', customDetailsController.customFieldDetails)
-customDetailsRouter.get('/', customDetailsController.getAllCustomfieldDetails)
-customDetailsRouter.get('/:id', customDetailsController.getCustomFieldById)
-customDetailsRouter.delete('/:id', customDetailsController.deleteCustomFields)
-
-module.exports = customDetailsRouter;
-=======
 const express = require("express");
 const customDetailsRouter = express.Router();
 const customDetailsController = require("../../controller/admin/staff/customDetails.controller");
@@ -21,4 +9,3 @@ customDetailsRouter.delete("/:id", customDetailsController.deleteCustomFields);
 customDetailsRouter.put("/:id", customDetailsController.updateCustomFields);
 
 module.exports = customDetailsRouter;
->>>>>>> 2a6ebb8629ac2da4206638ea938b9f79a28bf21d:routes/admin/customDetails.router.js
