7fe05bce779e0e113f98743da69d0f78ac7fcca4
Shukal-01 <131428954+Shukal-01@users.noreply.github.com>
2024-10-22 10:42:20 +0530

    Task Status table updated

---
+++ prisma/migrations/20241022051027_update_task_status_table/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "TaskStatus" DROP CONSTRAINT "TaskStatus_canBeChangedId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "TaskStatus" DROP CONSTRAINT "TaskStatus_isHiddenId_fkey";
