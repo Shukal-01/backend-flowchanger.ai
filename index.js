2555e4c969bb0f59f8852fcedb38973829127714
Shukal-01 <131428954+Shukal-01@users.noreply.github.com>
2024-12-06 02:12:51 +0530

    change in index.js file

--- index.js
+++ index.js
@@ -10,14 +10,11 @@ const path = require("path");
 const app = express();
 const _dirname = path.dirname("")
 const buildpath = path.join(_dirname, "../frontend-flowchanger.ai/build")
-// app.use(express.static(buildpath))
+app.use(express.static(buildpath))
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html')); // Absolute path to index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildpath, 'index.html'));
 });
-
 const PORT = process.env.PORT || 4000;
 
 app.use(
