e16ed2fa9fc1607e8557a47535d352eaefd7fcde
Shukal-01 <131428954+Shukal-01@users.noreply.github.com>
2024-12-05 17:19:59 +0530

    add ecosystem.config.js file

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+        {
+            name: "backend-flowchanger.ai",
+            script: "index.js",
+            env: {
+                PORT: 5000,
+            },
+        },
+    ],
+};
