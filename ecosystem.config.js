229772ee0e9ad46f213922f83be6ac978e896853
Shukal-01 <131428954+Shukal-01@users.noreply.github.com>
2024-12-05 23:23:03 +0530

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,16 @@
 module.exports = {
     apps: [
         {
-            name: "backend-flowchanger.ai",
-            script: "index.js",
+            name: "my-backend-app",
+            script: "index.js", // Entry point file of your app
+            instances: "max", // Number of instances, "max" uses all available CPUs
+            exec_mode: "cluster", // Use cluster mode for better performance
             env: {
+                NODE_ENV: "development",
+                PORT: 5000,
+            },
+            env_production: {
+                NODE_ENV: "production",
                 PORT: 5000,
             },
         },
