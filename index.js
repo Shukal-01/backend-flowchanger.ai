21624833c9ec24af4757b39897ee64a8f3c425a1
Shukal-01 <131428954+Shukal-01@users.noreply.github.com>
2024-12-08 11:04:09 +0530

    change

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(buildpath, 'index.html'));
 // });
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 // const PORT = 5000;
 app.use(
   cors({
